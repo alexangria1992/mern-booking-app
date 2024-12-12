d5a4b121a259a0ec59ea6cc430c60b9366e29e83
Alexandre Angrignon <alex040892@gmail.com>
2024-12-12 19:19:53 +1300

    Database Connection and Vite Initialiazation

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,13 @@ import express, { Request, Response } from 'express';
 import cors from 'cors';
 import colors from 'colors';
 import 'dotenv/config';
-const bodyParser = require('body-parser');
+import mongoose from 'mongoose';
+
+mongoose.connect(
+  `${process.env.MONGODB_CONNECTION_STRING as string} ${console.log(
+    colors.magenta('Database is connected')
+  )} `
+);
 
 const app = express();
 app.use(express.json());
