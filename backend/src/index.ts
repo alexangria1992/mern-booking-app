c50d5646ec963b48f98068f9fe89d2d256907934
Alexandre Angrignon <alex040892@gmail.com>
2024-12-20 12:48:03 +1300

    Backend Login User

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,8 @@ import colors from 'colors';
 import 'dotenv/config';
 import mongoose from 'mongoose';
 import userRoutes from './routes/users';
+import authRoutes from './routes/auth';
+
 mongoose.connect(
   `${process.env.MONGODB_CONNECTION_STRING as string} ${console.log(
     colors.magenta('Database is connected')
@@ -15,6 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 // app.get('/api/test', async (req: Request, res: Response) => {
 //   res.json({ message: 'hello from express endpoint!' });
