f0c5347099a8c123c9f054b6aae951044d7f368f
Alexandre Angrignon <alex040892@gmail.com>
2024-12-20 20:47:21 +1300

    App Context & Toast Notifications

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,6 +1,8 @@
 import { useForm } from 'react-hook-form';
 import { useMutation } from 'react-query';
 import * as apiClient from '../api-client';
+import { useAppContext } from '../contexts/AppContext';
+import { useNavigate } from 'react-router-dom';
 
 export type RegisterFormData = {
   firstName: string;
@@ -10,6 +12,8 @@ export type RegisterFormData = {
   confirmPassword: string;
 };
 const Register = () => {
+  const navigate = useNavigate();
+  const { showToast } = useAppContext();
   const {
     register,
     watch,
@@ -19,10 +23,13 @@ const Register = () => {
 
   const mutation = useMutation(apiClient.register, {
     onSuccess: () => {
-      console.log('registration successful');
+      //   console.log('registration successful');
+      showToast({ message: 'Registration Success!', type: 'SUCCESS' });
+      navigate('/');
     },
     onError: (error: Error) => {
-      console.log(error.message);
+      //   console.log(error.message);
+      showToast({ message: error.message, type: 'ERROR' });
     },
   });
 
